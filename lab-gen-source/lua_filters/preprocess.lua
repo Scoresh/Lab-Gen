b64cba8eb582cbcc0ae0f56cfc076b062fd74f1f
daniel_sab <danielsabalakov@gmail.com>
2025-09-03 14:00:48 -0400

    mid school push

--- lab-gen-source/lua_filters/preprocess.lua
+++ lab-gen-source/lua_filters/preprocess.lua
@@ -31,7 +31,7 @@ function Pandoc(doc)
 
     end
     -- insert title content now that the commands are defined. CHANGE LATER
-    table.insert(returnTable,pandoc.RawInline("latex","\\input{basetemplate.tex}"))
+    table.insert(returnTable,pandoc.RawInline("latex","\\input{latex_templates/basetitle.tex}"))
 
     doc.blocks = pandoc.List(returnTable) .. doc.blocks
 
